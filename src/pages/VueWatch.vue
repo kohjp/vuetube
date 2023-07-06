ea2114156d8899a6d3f5e07e72c9c6d2bb21fdc0
kohjp <rshnms12@gmail.com>
2023-07-06 22:44:27 +0900

    Feat: Add watch page detail

--- src/pages/VueWatch.vue
+++ src/pages/VueWatch.vue
@@ -16,13 +16,29 @@
       <span class="item-title">
         {{ item.snippet?.title }}
       </span>
-      <div>
+      <div class="channel">
         <img
           class="channel-thumbnail"
           :src="channel.snippet.thumbnails.default.url"
           alt="channel-thumbnail"
           v-if="!isLoading"
         />
+        <div class="channel-info">
+          <span class="channel-title">
+            {{ channel.snippet?.title }}
+          </span>
+          <span class="channel-subscriber">
+            구독자 {{ channel.statistics?.subscriberCount.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") }} 명
+          </span>
+        </div>
+      </div>
+      <div class="item-detail">
+        <span>
+           조회수 {{  item.statistics?.viewCount.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") }}회
+        </span>
+        <div>
+            {{ item.snippet?.description }}
+        </div>
       </div>
     </div>
   </div>
@@ -56,10 +72,11 @@ export default {
         });
       await axios
         .get(
-          `https://www.googleapis.com/youtube/v3/channels?key=${process.env.VUE_APP_API_KEY}&part=snippet&id=${item.value.snippet.channelId}`
+          `https://www.googleapis.com/youtube/v3/channels?key=${process.env.VUE_APP_API_KEY}&part=snippet,statistics&id=${item.value.snippet.channelId}`
         )
         .then((res) => {
           channel.value = res.data.items[0];
+          console.log(channel.value);
           isLoading.value = false;
         });
     })();
@@ -73,11 +90,14 @@ export default {
 .container {
   display: flex;
   flex-direction: column;
+  width: 100%;
+  padding-right: 90px;
+  margin-top: 30px;
 }
 
 .video {
   width: 100%;
-  height: 70vh;
+  height: 80vh;
 }
 
 .item-info {
@@ -89,9 +109,38 @@ export default {
   font-weight: 700;
 }
 
+.channel {
+  margin-top: 10px;
+  display: flex;
+  gap: 10px;
+}
+
 .channel-thumbnail {
   width: 45px;
   height: 45px;
   border-radius: 50%;
 }
+
+.channel-title {
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+.channel-info {
+  display: flex;
+  flex-direction: column;
+}
+
+.item-detail{
+    background: #E5E5E5;
+    padding: 20px;
+    border-radius: 15px;
+    width: 100%;
+    margin-top: 10px;
+    margin-bottom: 30px;
+    font-weight: 500;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
 </style>
