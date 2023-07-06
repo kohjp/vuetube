472916820b6b2dfa83b57f8603be79b04c9129ee
kohjp <rshnms12@gmail.com>
2023-07-06 20:54:35 +0900

    Feat: Async get API

--- src/pages/VueWatch.vue
+++ src/pages/VueWatch.vue
@@ -12,17 +12,59 @@
       >
       </iframe>
     </div>
+    <div class="item-info">
+      <span class="item-title">
+        {{ item.snippet?.title }}
+      </span>
+      <div>
+        <img
+          class="channel-thumbnail"
+          :src="channel.snippet.thumbnails.default.url"
+          alt="channel-thumbnail"
+          v-if="!isLoading"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import { reactive, ref } from "vue";
+import { useRoute } from "vue-router";
 export default {
   setup() {
-    const url = "";
-    return { url };
-  },
-  beforeMount() {
-    this.url = `https://www.youtube.com/embed/${this.$route.params.id}?autoplay=1&mute=1`;
+    const data = reactive({
+      item: {},
+      channel: {},
+    });
+    const url = ref("");
+    const item = ref({});
+    const channel = ref({});
+    const route = useRoute();
+    const isLoading = ref(true);
+
+    url.value = `https://www.youtube.com/embed/${route.params.id}?autoplay=0&mute=0`;
+
+    (async () => {
+      await axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/videos?key=${process.env.VUE_APP_API_KEY}&part=snippet,statistics&id=${route.params.id}&regionCode=kr`
+        )
+        .then((res) => {
+          item.value = res.data.items[0];
+        });
+      await axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/channels?key=${process.env.VUE_APP_API_KEY}&part=snippet&id=${item.value.snippet.channelId}`
+        )
+        .then((res) => {
+          channel.value = res.data.items[0];
+          isLoading.value = false;
+        });
+    })();
+
+    return { url, data, channel, item, isLoading };
   },
 };
 </script>
@@ -30,8 +72,26 @@ export default {
 <style scoped>
 .container {
   display: flex;
+  flex-direction: column;
 }
 
 .video {
+  width: 100%;
+  height: 70vh;
+}
+
+.item-info {
+  margin-top: 20px;
+}
+
+.item-info .item-title {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.channel-thumbnail {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
 }
 </style>
